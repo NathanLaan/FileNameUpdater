53e13be3dc6a5cb03aa9748899e569b0e6162f58
Nathan Laan <nathanlaan@gmail.com>
2013-11-28 15:11:04 -0700

    Added basic thread logic to file update engine.

--- FileNameUpdater.App/UpdateEngine.cs
+++ FileNameUpdater.App/UpdateEngine.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace FileNameUpdater.App
 {
@@ -10,6 +10,48 @@ namespace FileNameUpdater.App
     {
 
         public string Path { get; set; }
+        private Thread thread;
+        private bool threadRunning = false;
+
+        public UpdateEngine()
+        {
+        }
+
+        public void Start()
+        {
+            this.threadRunning = true;
+            //Initialize Thread
+            if (null == this.thread)
+            {
+                this.thread = new Thread(new ThreadStart(this.Run));
+            }
+
+            this.thread.Start();
+        }
+
+        public void Stop()
+        {
+            this.threadRunning = false;
+            this.thread.Abort();
+        }
+
+        private void Run()
+        {
+        }
+
+        // Event code
+
+        public delegate void FileUpdateEvent(string oldFileName, string newFileName);
+
+        public event FileUpdateEvent FileNameChanged;
+
+        private void OnFileNameChanged(string oldFileName, string newFileName)
+        {
+            if (this.FileNameChanged != null)
+            {
+                this.FileNameChanged(oldFileName, newFileName);
+            }
+        }
 
     }
 }
